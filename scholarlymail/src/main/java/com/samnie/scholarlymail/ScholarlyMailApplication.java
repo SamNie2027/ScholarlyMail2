472fd59b957315cc204809d244f6f6a76d878ec7
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2025-10-24 12:26:59 -0400

    Add scholarlymail directory

---
+++ scholarlymail/src/main/java/com/samnie/scholarlymail/ScholarlyMailApplication.java
@@ -0,0 +1,13 @@
+package com.samnie.scholarlymail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarlyMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarlyMailApplication.class, args);
+	}
+
+}
---
+++ scholarlymail/src/test/java/com/samnie/scholarlymail/ScholarlymailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samnie.scholarlymail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarlymailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
