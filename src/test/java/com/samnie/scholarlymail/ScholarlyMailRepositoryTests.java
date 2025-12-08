51544e6ed8f377ff09ce94da66461300c31c3829
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2025-12-08 15:22:47 -0500

    Disable integration tests that require Couchbase DB in CI

--- src/test/java/com/samnie/scholarlymail/ScholarlyMailRepositoryTests.java
+++ src/test/java/com/samnie/scholarlymail/ScholarlyMailRepositoryTests.java
@@ -1,5 +1,6 @@
 package com.samnie.scholarlymail;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("Test")
+@Disabled("Requires local Couchbase instance - run manually with local DB")
 class ScholarlyMailRepositoryTests {
 
     @Autowired
